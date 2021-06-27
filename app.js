86bd3902e6e3456f5d208d6dbca53216317994fd
niketan124 <70933660+niketan124@users.noreply.github.com>
2021-06-27 20:16:46 +0530

    Updates navbar on each page and updated pages
    Updated navbar on each page and added styling to each page. And added parallax design to few pages.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+// const date = new Date();
+// const thisYear = date.getFullYear();
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -14,6 +17,17 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+app.get("/products", function (req, res) {
+  res.render("products");
+});
+
+app.get("/signUp", function (req, res) {
+  res.render("signUp");
+});
+
+app.post("/signUp", function (req, res) {
+  res.render("signUp");
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
